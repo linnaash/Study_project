73755595eae87da4a2ea33535f434e99b9f4f39f
linnaash <allschagieva@gmail.com>
2024-09-11 14:27:57 +0300

    update

---
+++ lab1_api/lab1_api/Event.cs
@@ -0,0 +1,10 @@
+namespace lab1_api
+{
+    public class Event
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
