1931fc7f79c3a2b4ac03171218cd9261caa9dee6
linnaash <allschagieva@gmail.com>
2024-10-15 13:29:09 +0300

    upd

--- Cultura/Cultura/obj/Debug/net6.0/Cultura-Test.AssemblyInfo.cs
+++ Cultura/Cultura/obj/Debug/net6.0/Cultura-Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cultura-Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f96a2913ffe9cac86772689956aaf6ab9c3439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84188f7e47adb002a8497e61ae4b87887f68d8bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cultura-Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cultura-Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
